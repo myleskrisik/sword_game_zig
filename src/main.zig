d15d2a7ed654fb1320774063153b2e064e0f2354
myleskrisik <myleskrisik@gmail.com>
2024-06-17 09:00:32 -0700

    Got movement working pretty good

--- src/main.zig
+++ src/main.zig
@@ -22,6 +22,31 @@ const Subpixel = struct {
     pub fn from_float(float: f32) Subpixel {
         return Subpixel{ .floating_point_representation = @intFromFloat(float * SCALING_FACTOR) };
     }
+
+    pub fn sign(self: Subpixel) Subpixel {
+        return .{ .floating_point_representation = std.math.sign(self.floating_point_representation) };
+    }
+
+    pub fn minus(self: Subpixel, other: Subpixel) Subpixel {
+        return .{ .floating_point_representation = self.floating_point_representation - other.floating_point_representation };
+    }
+
+    pub fn plus(self: Subpixel, other: Subpixel) Subpixel {
+        return .{ .floating_point_representation = self.floating_point_representation + other.floating_point_representation };
+    }
+
+    pub fn times(self: Subpixel, other: Subpixel) Subpixel {
+        return .{ .floating_point_representation = self.floating_point_representation * other.floating_point_representation };
+    }
+
+    pub fn move_towards(self: *Subpixel, target: Subpixel, max_delta: Subpixel) void {
+        const distance_from_target = target.minus(self.*);
+        if (@abs(distance_from_target.floating_point_representation) <= max_delta.floating_point_representation) {
+            self.* = target;
+            return;
+        }
+        self.* = self.plus(distance_from_target.sign().times(max_delta));
+    }
 };
 
 const Position = struct { x: Subpixel = Subpixel.from_int(0), y: Subpixel = Subpixel.from_int(0) };
@@ -48,10 +73,19 @@ const Velocity = struct {
     }
 };
 
+const Acceleration = struct {
+    x: Subpixel = Subpixel.from_int(0),
+    y: Subpixel = Subpixel.from_int(0),
+
+    pub fn init(x: Subpixel, y: Subpixel) Acceleration {
+        return .{ x, y };
+    }
+};
+
 const Player = struct {
-    const ACCELERATION = 2;
-    const CHANGING_DIRECTION_ACCELERATION = 40;
-    const FRICTION = 3;
+    const ACCELERATION = 4;
+    const CHANGING_DIRECTION_ACCELERATION = 4;
+    const FRICTION = 10;
     const MAX_VELOCITY = 240;
 
     position: Position,
@@ -80,34 +114,47 @@ const Player = struct {
         }
 
         const vec = rl.Vector2{ .x = x_input, .y = y_input };
-        const squared_elements_sum = if (vec.x == 0.0) 0 else std.math.pow(f32, vec.x, 2) + if (vec.y == 0.0) 0 else std.math.pow(f32, vec.y, 2);
+        const x_squared = if (vec.x == 0.0) 0 else std.math.pow(f32, vec.x, 2);
+        const y_squared = if (vec.y == 0.0) 0 else std.math.pow(f32, vec.y, 2);
+        const squared_elements_sum = x_squared + y_squared;
         const vec_magnitude = @sqrt(squared_elements_sum);
+
         return if (vec_magnitude == 0) .{ 0, 0 } else .{ vec.x / vec_magnitude, vec.y / vec_magnitude };
     }
 
-    pub fn calculate_acceleration(self: Player, inputs: [2]f32) [2]Subpixel {
-        var accel: [2]Subpixel = .{ .{}, .{} };
+    pub fn calculate_acceleration(self: Player, inputs: [2]f32) Acceleration {
+        var accel: Acceleration = .{};
         const changing_directions = self.velocity.changing_direction(inputs);
-        if (@abs(self.position.x.floating_point_representation) < Player.MAX_VELOCITY) {
-            accel[0].floating_point_representation = if (changing_directions[0]) Player.CHANGING_DIRECTION_ACCELERATION else Player.ACCELERATION;
-            accel[0].floating_point_representation *= Subpixel.from_float(inputs[0]).floating_point_representation;
+        if (@abs(self.velocity.x.floating_point_representation) < Player.MAX_VELOCITY or changing_directions[0]) {
+            accel.x.floating_point_representation = if (changing_directions[0]) Player.CHANGING_DIRECTION_ACCELERATION else Player.ACCELERATION;
+            accel.x = accel.x.times(Subpixel.from_float(inputs[0]));
         }
 
-        if (@abs(self.position.x.floating_point_representation) < Player.MAX_VELOCITY) {
-            accel[1].floating_point_representation = if (changing_directions[1]) Player.CHANGING_DIRECTION_ACCELERATION else Player.ACCELERATION;
-            accel[1].floating_point_representation *= Subpixel.from_float(inputs[1]).floating_point_representation;
+        if (@abs(self.velocity.y.floating_point_representation) < Player.MAX_VELOCITY or changing_directions[1]) {
+            accel.y.floating_point_representation = if (changing_directions[1]) Player.CHANGING_DIRECTION_ACCELERATION else Player.ACCELERATION;
+            accel.y = accel.y.times(Subpixel.from_float(inputs[1]));
         }
         return accel;
     }
 
-    pub fn apply_acceleration(self: *Player, acceleration: [2]Subpixel) void {
-        self.velocity.x.floating_point_representation += acceleration[0].floating_point_representation;
-        self.velocity.y.floating_point_representation += acceleration[1].floating_point_representation;
+    pub fn apply_acceleration(self: *Player, acceleration: Acceleration, delta_time: f32) void {
+        const delta_time_subpixel = 
+        self.velocity.x = acceleration.x.plus(self.velocity.x).times();
+        self.velocity.y = acceleration.y.plus(self.velocity.y);
     }
 
     pub fn update_position(self: *Player) void {
         self.position.x.floating_point_representation += self.velocity.x.floating_point_representation;
-        self.position.y.floating_point_representation += self.velocity.x.floating_point_representation;
+        self.position.y.floating_point_representation += self.velocity.y.floating_point_representation;
+    }
+
+    pub fn apply_friction(self: *Player, inputs: [2]f32) void {
+        if (inputs[0] == 0) {
+            self.velocity.x.move_towards(.{ .floating_point_representation = 0 }, .{ .floating_point_representation = Player.FRICTION });
+        }
+        if (inputs[1] == 0) {
+            self.velocity.y.move_towards(.{ .floating_point_representation = 0 }, .{ .floating_point_representation = Player.FRICTION });
+        }
     }
 };
 
@@ -144,6 +191,7 @@ pub fn main() !void {
     defer rl.UnloadTexture(player_sprite);
 
     while (!rl.WindowShouldClose()) {
+        const delta_time = rl.GetFrameTime();
         // Update
         {
             screen_space_camera.target = rl.Vector2{ .x = camera_x, .y = camera_y };
@@ -158,9 +206,10 @@ pub fn main() !void {
 
             const inputs = Player.get_movement_inputs();
             const acceleration = player.calculate_acceleration(inputs);
-            std.debug.print("inputs {any} acceleration {any}\n", .{ inputs, acceleration });
-            player.apply_acceleration(acceleration);
-            player.update_position();
+            player.apply_acceleration(acceleration, delta_time);
+            player.apply_friction(inputs, delta_time);
+            player.update_position(delta_time);
+            std.log.info("velocity: (x: {any}, y: {any})", .{ player.velocity.x.floating_point_representation, player.velocity.y.floating_point_representation });
         }
         // Draw worldspace
         {
@@ -169,7 +218,7 @@ pub fn main() !void {
             rl.ClearBackground(rl.WHITE);
             {
                 rl.BeginMode2D(world_space_camera);
-                rl.DrawTexture(player_sprite, player.position.x.as_int(), player.position.x.as_int(), rl.WHITE);
+                rl.DrawTexture(player_sprite, player.position.x.as_int(), player.position.y.as_int(), rl.WHITE);
                 defer rl.EndMode2D();
             }
         }
